878e71f304f8d21e87099771eaa0215edf9df29e
Vadim <sssd2.makemoney@gmail.com>
2020-12-06 18:44:49 +0200

    update

--- lesson 2/task1/main.cpp
+++ lesson 2/task1/main.cpp
@@ -12,19 +12,20 @@ int main ()
       cout << "Enter school number: ";
       cin >> school[num];
     }
-  cout << "Now Enter your school number: ";
-  cin >> mySchool;
+      cout << "Now Enter your school number: ";
+      cin >> mySchool;
   for (int num = 0; num < 10; num++)
     {
-      if (school[num] == mySchool)
-	{
+  if (school[num] == mySchool)
+    {
 	  cout << "I know this school!";
 	  break;
 	}
-      else if (num == 9)
+  else if (num == 9)
 	{
 	  cout << "I don't know this school.";
 	}
     }
   return 0;
 }
+
